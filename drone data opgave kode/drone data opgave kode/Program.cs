702571caa251214c0cd7a9f59f237315569a81de
U1umos <victorsmjensen@gmail.com>
2023-08-18 07:50:24 +0200

    start of hell

---
+++ drone data opgave kode/drone data opgave kode/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace drone_data_opgave_kode
+{
+using System.IO;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string readText = File.ReadAllText("flyvning1.csv");
+            Console.WriteLine(readText);
+        }
+    }
+}
