74e0a98d2f9fb9a740a93e384c65d8b78f526b59
U1umos <victorsmjensen@gmail.com>
2023-08-18 08:55:38 +0200

    split kinda working
    mangler at fikse foreach loopet om til et for loop

--- drone data opgave kode/drone data opgave kode/Program.cs
+++ drone data opgave kode/drone data opgave kode/Program.cs
@@ -7,6 +7,21 @@ using System.IO;
         {
             string readText = File.ReadAllText("flyvning1.csv");
             Console.WriteLine(readText);
+            Console.ReadLine();
+
+            String[] spearator = { ";"};
+            Int32 count = int.MaxValue;
+
+            // using the method
+            String[] datalist = readText.Split(spearator, count,
+                   StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (String s in datalist)
+            {
+                Console.WriteLine(s);
+            }
         }
+  
     }
+
 }
