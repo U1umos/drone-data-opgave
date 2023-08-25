e2b365d1f106c6373b0561b796a1c4a0eb6a5ecb
U1umos <victorsmjensen@gmail.com>
2023-08-25 13:41:47 +0200

    Update Program.cs

--- drone data opgave kode/drone data opgave kode/Program.cs
+++ drone data opgave kode/drone data opgave kode/Program.cs
@@ -5,23 +5,32 @@ using System.IO;
     {
         static void Main(string[] args)
         {
+            File.Delete("Sorted.csv");
             string readText = File.ReadAllText("flyvning1.csv");
             Console.WriteLine(readText);
             Console.ReadLine();
 
-            String[] spearator = { ";"};
+            String[] spearator = { ";","\n"};
             Int32 count = int.MaxValue;
 
             // using the method
             String[] datalist = readText.Split(spearator, count,
                    StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (String s in datalist)
+            for (int i = 0; i<datalist.Length; i++)
             {
-                Console.WriteLine(s);
+                Console.WriteLine(datalist[i]);
+                File.AppendAllText("sorted.csv", datalist[i] + "\n");
+            }
+
+            string[] longData = new string[datalist.Length];
+            for(int i = 17; i<datalist.Length;i++)
+            {
+                string BrokenLongData = datalist[i];
+                Console.WriteLine(BrokenLongData);
+                i = (i + 12);
+                break;
             }
         }
-  
     }
-
 }
