82386e385d26a301baefbb6f9fb075993b3fc940
U1umos <victorsmjensen@gmail.com>
2023-09-13 08:45:15 +0200

    start på ny sammensæt

--- drone data opgave kode/drone data opgave kode/Program.cs
+++ drone data opgave kode/drone data opgave kode/Program.cs
@@ -17,20 +17,14 @@ using System.IO;
             String[] datalist = readText.Split(spearator, count,
                    StringSplitOptions.RemoveEmptyEntries);
 
-            for (int i = 0; i<datalist.Length; i++)
-            {
-                Console.WriteLine(datalist[i]);
-                File.AppendAllText("sorted.csv", datalist[i] + "\n");
-            }
-
             string[] longData = new string[datalist.Length];
             for(int i = 17; i<datalist.Length;i=i+13)
             {
                 string BrokenLongData = datalist[i];
                 BrokenLongData = BrokenLongData.Replace(".", string.Empty);
-                Console.WriteLine(BrokenLongData.Replace(".", string.Empty));
+                /*Console.WriteLine(BrokenLongData.Replace(".", string.Empty));
                 Console.WriteLine(i / 13);
-                Console.WriteLine(BrokenLongData.Insert(1,"."));
+                Console.WriteLine(BrokenLongData.Insert(1,"."));*/
                 BrokenLongData = BrokenLongData.Insert(2, ".");
                 longData[i] = BrokenLongData;                
             }
@@ -39,14 +33,20 @@ using System.IO;
             {
                 string BrokenLatData = datalist[i];
                 BrokenLatData = BrokenLatData.Replace(".", string.Empty);
-                Console.WriteLine(BrokenLatData.Replace(".",string.Empty));
+               /* Console.WriteLine(BrokenLatData.Replace(".",string.Empty));
                 Console.WriteLine(i / 13);
                 Console.WriteLine(BrokenLatData);
-                Console.WriteLine(BrokenLatData.Insert(2, "."));
+                Console.WriteLine(BrokenLatData.Insert(2, "."));*/
                 BrokenLatData = BrokenLatData.Insert(2, ".");
                 latData[i] = BrokenLatData;
             }
-
+            string line = "";
+            for (int i = 0; i < datalist.Length; i++)
+            {
+                line = datalist[i] + datalist[i+1] + datalist[i + 2] + datalist[i + 3] + datalist[i + 4] + datalist[i + 5] + datalist[i + 6] + datalist[i + 7] + datalist[i + 8] + datalist[i + 9] + datalist[i + 10] + datalist[i + 11] + datalist[i + 12] + "GPSKoord";
+                // Console.WriteLine(datalist[i]);
+                File.AppendAllText("sorted.csv", datalist[i] + "\n");
+            }
         }
     }
 }
